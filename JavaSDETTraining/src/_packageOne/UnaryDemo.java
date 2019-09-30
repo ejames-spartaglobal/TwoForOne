2c90af103b161ff5fa5849976afca69e6889b139
Edward James <ejames@spartaglobal.com>
2019-09-30 14:08:06 +0100

    Commit - Unary Demo

---
+++ JavaSDETTraining/src/_packageOne/UnaryDemo.java
@@ -0,0 +1,22 @@
+package _packageOne;
+
+public class UnaryDemo
+{
+    public static void main(String[] args) {
+        int result = +1;
+        System.out.println(result);
+
+        result--;
+        System.out.println(result);
+
+        result++;
+        System.out.println(result);
+
+        result= -result;
+        System.out.println(result);
+
+        boolean success = false;
+        System.out.println(success);
+        System.out.println(!success);
+    }
+}
