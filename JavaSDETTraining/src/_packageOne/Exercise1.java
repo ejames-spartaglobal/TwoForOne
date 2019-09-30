b87520969e62ac4a0a2d7c35427f5812343e0713
Edward James <ejames@spartaglobal.com>
2019-09-30 11:42:11 +0100

    Commit - ArrayDemo 1-3

---
+++ JavaSDETTraining/src/_packageOne/Exercise1.java
@@ -0,0 +1,19 @@
+package _packageOne;
+
+import java.util.Arrays;
+
+public class Exercise1
+{
+    public static void main(String[] args) {
+        char[] firstArray = {'p','a','s','s','w','o','r','d'};
+
+        char[] thirdArray = new char[firstArray.length];
+        char[] secondArray= Arrays.copyOfRange(firstArray, 4,firstArray.length);
+        System.arraycopy(firstArray, 4, thirdArray,4,firstArray.length-4);
+
+        System.out.println(secondArray);
+        System.out.println(thirdArray);
+    }
+
+
+}
