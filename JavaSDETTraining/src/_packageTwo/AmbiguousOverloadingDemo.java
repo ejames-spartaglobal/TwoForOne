8200ea097b5a2efb7dd84e2e94ec266073e43ba2
Edward James <ejames@spartaglobal.com>
2019-10-01 18:03:46 +0100

    Commit - AmbiguousOverloading Demos updated and SortingExercise added

--- JavaSDETTraining/src/_packageTwo/AmbiguousOverloadingDemo.java
+++ JavaSDETTraining/src/_packageTwo/AmbiguousOverloadingDemo.java
@@ -6,15 +6,15 @@ public class AmbiguousOverloadingDemo
         if(num1>num2){
             return num1;
         }
-        else {return num2}
+        else {return num2;}
     }
 
     public  static double max(double num1, int num2){
         if(num1>num2){return  num1;}
-        else{return num2}
+        else{return num2;}
     }
 
     public static void main(String[] args) {
-        System.out.println(max(1,2));
+        System.out.println(max(1.2,2));
     }
 }
---
+++ JavaSDETTraining/src/_packageTwo/SortingExercise.java
@@ -0,0 +1,49 @@
+package _packageTwo;
+
+public class SortingExercise {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 6;
+        int num3 = 7;
+
+        sort(num3,num2,num1);
+
+    }
+
+    public static void sort(int n1, int n2, int n3){
+        if(n1<n2 && n1<n3){
+            if(n2>n3){
+        int temp = n3;
+        n1=n2;
+        n2=n3;
+        n1=temp;
+        }
+            else {    int temp = n3;
+                n3=n1;
+                n2=n2;
+                n3=temp;
+            }
+        }
+
+        else if(n1<n3){
+            int temp = n3;
+            n3=n1;
+            n1=temp;
+        }
+       /* else if(n2>n1){
+            int temp2 = n1;
+            n1=n2;
+            n2=temp2;
+        }*/
+            if(n3>n2){
+            int temp = n3;
+            n3=n2;
+            n2=temp;
+        }
+
+
+
+        System.out.println(n3 + " " + n2 + " " + n1);
+
+    }
+}
